d3db5795d982a9bbb0901a0a66e109368ecdc748
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-04-01 23:35:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LetterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            var Get_Text = Console.ReadLine();
+            
+            foreach(var value in Get_Text)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
